f52b41d05b5b4b6aade001b6a3833aa3acfa8423
minahany16 <mina309hany@gmail.com>
2023-09-09 03:03:52 +0300

    finish here

--- FE/src/pages/Login.js
+++ FE/src/pages/Login.js
@@ -4,9 +4,10 @@ import Button from "@mui/material/Button";
 import { useState } from "react";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 export default function Login() {
+  const[ispending,setIspending] = useState(true);
+  const[handlelog,sethandlelog] = useState(false);
   const [showPass] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -18,7 +19,6 @@ export default function Login() {
   };
 
   const handleClick = async (e) => {
-    e.preventDefault();
     const loginTry = { email, password };
     console.log(loginTry);
     try {
@@ -37,6 +37,7 @@ export default function Login() {
         .then((data) => {
           localStorage.setItem("userId", data.user._id);
           localStorage.setItem("logged", "true");
+          setIspending(true);
         });
     } catch (error) {
       console.error(error);
@@ -45,45 +46,54 @@ export default function Login() {
       refresh();
     }, 200);
   };
+  const  handlelogin= () => {
+    setIspending(false);
+    handleClick();
+      sethandlelog(true);
+  }
+   
   return (
     <div>
-      <Paper
-        elevation={10}
-        style={{ padding: "25px 10px", width: "50%", margin: "20px auto" }}
-      >
-        <h2>Login Page</h2>
-        <OutlinedInput
-          style={{ padding: "5px", margin: "5px" }}
-          placeholder="Email"
-          fullWidth={true}
-          size="small"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <OutlinedInput
-          style={{ padding: "5px", margin: "5px" }}
-          fullWidth={true}
-          placeholder="Password"
-          size="small"
-          onChange={(e) => setPassword(e.target.value)}
-          type={showPass ? "text" : "password"}
-        />
-        <Button
-          fullWidth={true}
-          variant="contained"
-          style={{ margin: "5px 5px" }}
-          onClick={handleClick}
+      <div className={ispending!==true ? "login hide":"login" }>
+        <Paper
+          elevation={10}
+          style={{ padding: "25px 10px", width: "50%", margin: "20px auto" }}
         >
-          <Link to="/Profile">Login</Link>
-        </Button>
-        <Button
-          fullWidth={true}
-          variant="contained"
-          style={{ margin: "10px 5px", backgroundColor: "green" }}
-          onClick={() => navigate("/Register")}
-        >
-          Create New Account
-        </Button>
-      </Paper>
+          <h2>Login Page</h2>
+          <OutlinedInput
+            style={{ padding: "5px", margin: "5px" }}
+            placeholder="Email"
+            fullWidth={true}
+            size="small"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <OutlinedInput
+            style={{ padding: "5px", margin: "5px" }}
+            fullWidth={true}
+            placeholder="Password"
+            size="small"
+            onChange={(e) => setPassword(e.target.value)}
+            type={showPass ? "text" : "password"}
+          />
+          <Button
+            fullWidth={true}
+            variant="contained"
+            style={{ margin: "5px 5px" }}
+            onClick={handlelogin}
+          > Login </Button>
+          <Button
+            fullWidth={true}
+            variant="contained"
+            style={{ margin: "10px 5px", backgroundColor: "green" }}
+            onClick={() => navigate("/Register")}
+          >
+            Create New Account
+          </Button>
+        </Paper>
+      </div>
+      <div className={ispending===true ? "loading hide":"loading" }></div>
+      <div>{handlelog && ispending && navigate("/Profile")}</div>
     </div>
+    
   );
 }
